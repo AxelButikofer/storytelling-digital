373cb1c23541e108655eb77a9e62f9d60ba4984e
AxelButikofer <122967727+AxelButikofer@users.noreply.github.com>
2024-11-13 13:49:04 +0100

    changed "var" to "const", javascript

--- src/js/main.js
+++ src/js/main.js
@@ -4,48 +4,48 @@ gsap.registerPlugin(ScrollTrigger);
 
 // HEADER PARALLAX
 
-var parallaxContainer = document.querySelector(".parallax-container");
+const parallaxContainer = document.querySelector(".parallax-container");
 
 parallaxContainer.addEventListener("mousemove", function (event) {
-  var containerCoords = parallaxContainer.getBoundingClientRect();
+  const containerCoords = parallaxContainer.getBoundingClientRect();
 
-  var clX = event.clientX - containerCoords.left;
-  var clY = event.clientY - containerCoords.top;
+  const clX = event.clientX - containerCoords.left;
+  const clY = event.clientY - containerCoords.top;
 
-  var percX = (clX / containerCoords.width) * 2 - 1;
-  var percY = (clY / containerCoords.height) * 2 - 1;
+  const percX = (clX / containerCoords.width) * 2 - 1;
+  const percY = (clY / containerCoords.height) * 2 - 1;
 
   // layer 1
-  var deltaX1 =
+  const deltaX1 =
     (-(containerCoords.width * 1.1 - containerCoords.width) * percX) / 2;
-  var deltaY1 =
+  const deltaY1 =
     (-(containerCoords.height * 1.1 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-1").style.transform =
     "translate(" + deltaX1 + "px, " + deltaY1 + "px) scale(1.1)";
 
   // layer 2
-  var deltaX2 =
+  const deltaX2 =
     (-(containerCoords.width * 1.2 - containerCoords.width) * percX) / 2;
-  var deltaY2 =
+  const deltaY2 =
     (-(containerCoords.height * 1.2 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-2").style.transform =
     "translate(" + deltaX2 + "px, " + deltaY2 + "px) scale(1.2)";
 
   // layer 3
-  var deltaX3 =
+  const deltaX3 =
     (-(containerCoords.width * 1.4 - containerCoords.width) * percX) / 2;
-  var deltaY3 =
+  const deltaY3 =
     (-(containerCoords.height * 1.4 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-3").style.transform =
     "translate(" + deltaX3 + "px, " + deltaY3 + "px) scale(1.4)";
 
   // layer 4
-  var deltaX4 =
+  const deltaX4 =
     (-(containerCoords.width * 1.7 - containerCoords.width) * percX) / 2;
-  var deltaY4 =
+  const deltaY4 =
     (-(containerCoords.height * 1.7 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-4").style.transform =
