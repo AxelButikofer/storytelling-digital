f4ff2812a08224f23175ba36caca14388a63c4f4
AxelButikofer <122967727+AxelButikofer@users.noreply.github.com>
2024-11-14 17:13:28 +0100

    corrected the size of the layer in the parallax in bigger viewport

--- src/js/main.js
+++ src/js/main.js
@@ -17,36 +17,36 @@ parallaxContainer.addEventListener("mousemove", function (event) {
 
   // layer 1
   const deltaX1 =
-    (-(containerCoords.width * 1.1 - containerCoords.width) * percX) / 2;
+    (-(containerCoords.width * 1.02 - containerCoords.width) * percX) / 2;
   const deltaY1 =
-    (-(containerCoords.height * 1.1 - containerCoords.height) * percY) / 2;
+    (-(containerCoords.height * 1.02 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-1").style.transform =
     "translate(" + deltaX1 + "px, " + deltaY1 + "px) scale(1)";
 
   // layer 2
   const deltaX2 =
-    (-(containerCoords.width * 1.2 - containerCoords.width) * percX) / 2;
+    (-(containerCoords.width * 1.05 - containerCoords.width) * percX) / 2;
   const deltaY2 =
-    (-(containerCoords.height * 1.2 - containerCoords.height) * percY) / 2;
+    (-(containerCoords.height * 1.05 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-2").style.transform =
     "translate(" + deltaX2 + "px, " + deltaY2 + "px) scale(1.2)";
 
   // layer 3
   const deltaX3 =
-    (-(containerCoords.width * 1.4 - containerCoords.width) * percX) / 2;
+    (-(containerCoords.width * 1.05 - containerCoords.width) * percX) / 2;
   const deltaY3 =
-    (-(containerCoords.height * 1.4 - containerCoords.height) * percY) / 2;
+    (-(containerCoords.height * 1.05 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-3").style.transform =
     "translate(" + deltaX3 + "px, " + deltaY3 + "px) scale(1.5)";
 
   // layer 4
   const deltaX4 =
-    (-(containerCoords.width * 1.7 - containerCoords.width) * percX) / 2;
+    (-(containerCoords.width * 1.2 - containerCoords.width) * percX) / 2;
   const deltaY4 =
-    (-(containerCoords.height * 1.7 - containerCoords.height) * percY) / 2;
+    (-(containerCoords.height * 1.2 - containerCoords.height) * percY) / 2;
 
   document.querySelector(".layer-4").style.transform =
     "translate(" + deltaX4 + "px, " + deltaY4 + "px) scale(1.5)";
